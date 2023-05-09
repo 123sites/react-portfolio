8393035e882539fb6ecb7ab215e16956e565220a
CF <cfreitas12345@gmail.com>
2023-05-09 16:55:51 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-cyan-200">
+      <div className="rounded-lg">
+        <div className="text-5xl font-extrabold">
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+            Hello world
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
