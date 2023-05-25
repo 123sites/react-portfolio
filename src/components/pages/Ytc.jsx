751986ba251d334938ba39a6a2f529093b2ff298
CF <cfreitas12345@gmail.com>
2023-05-25 16:23:13 -0700

    Improved text color.

--- src/components/pages/Ytc.jsx
+++ src/components/pages/Ytc.jsx
@@ -9,7 +9,7 @@ const Ytc = () => {
 
       <div className="container text-center">
         <a href="https://www.youtube.com/channel/UCMNe2clJI6nf-rmXoVg01xw" target="_blank"
-          className="btn text-2xl text-pink-500 tracking-wider font-bold" rel="noreferrer">My YouTube Channel:</a>
+          className="btn text-2xl tracking-wider font-bold" rel="noreferrer">My YouTube Channel:</a>
         <iframe width="700" height="415" src="https://www.youtube.com/embed/1xaHwxBi8tU" title="YouTube video player" frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         {/* <div className="picture rounded-xl mx-auto w-2/3 md:w-full mt-5"> */}
