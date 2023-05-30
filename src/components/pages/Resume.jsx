ca686bed23352f6fc0399604d24ec0de59681296
CF <cfreitas12345@gmail.com>
2023-05-30 01:45:45 -0700

    Added margin and padding.

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -8,81 +8,83 @@ const resume = ({ fileUrl }) => {
   );
 };
 
-export default resume;
+const Resume = () => {
+  return (
+    <div className="max-w-screen-xl text-stone-100 text-xl mx-auto block items-center justify-center text-center pt-10 px-1 md:flex-col tracking-widest">
+      <header>
+        <h1 className="text-4xl  text-orange-400 font-bold">Chel Freitas</h1>
+        <br></br>
+        <h3 className="text-2xl  text-orange-400 font-bold">Front-end Developer</h3>
+        <br></br>
+        <h3 className="text-2xl  text-orange-400 font-bold">Downloadable Resume</h3>
+        <br></br>
+      </header>
 
+      <section>
+        <h2 className="text-2xl text-orange-400 font-bold">Skills</h2>
+        <ul>
+          <li>JavaScript, HTML, HTML5, NoSQL, Express OOP, Node.js, APIs, PWA, React.js, MERN</li>
+          <li>CSS, Bootstrap, Tailwind, and Responsive Web Design</li>
+        </ul>
+      </section>
 
-// const Resume = () => {
-//   return (
-//     <div className="resume">
-//       <header>
-//         <h1>Chel Freitas</h1>
-//         <h3>Front-end Developer</h3>
-//         <h3>Downloadable Resume</h3>
-//       </header>
+      <section>
+        <br></br>
+        <h2 className="text-2xl text-orange-400 font-bold">Experience</h2>
+        <div className="experience-item">
+          <h3 className="text-2xl">Content Creator</h3>
+          <h4>Self-employed</h4>
+          <p>2022 - present</p>
+          <ul>
+            <li>Creates videos about traveliing to the Azores and researching genealogy from there.</li>
+            <br></br>
+          </ul>
+        </div>
 
-//       <section>
-//         <h2>Skills</h2>
-//         <ul>
-//           <li>HTML5</li>
-//           <li>CSS</li>
-//           <li>Tailwind</li>
-//           <li>JavaScript</li>
-//           <li>React.js</li>
-//           <li>Responsive Web Design</li>
-//         </ul>
-//       </section>
+        <div className="experience-item">
+          <h3 className="text-2xl">Middle School Teacher</h3>
+          <h4>Tortolita Middle School</h4>
+          <p>2015 - 2022</p>
+          <ul>
+            <li>Taught computers, TV broadcasting, and science.</li>
+            <br></br>
+          </ul>
+        </div>
 
-//       <section>
-//         <h2>Experience</h2>
-//         <div className="experience-item">
-//           <h3>Content Creator</h3>
-//           <h4>Self-employed</h4>
-//           <p>2022 - present</p>
-//           <ul>
-//             <li>Creates videos about traveliing to the Azores and researching genealogy from there.</li>
-//           </ul>
-//         </div>
+        <div className="experience-item">
+          <h3 className="text-2xl">6th Grade Teacher</h3>
+          <h4>Ironwood Elementary School</h4>
+          <p>January 2019 - May 2019</p>
+          <ul>
+            <li>Taught most subject in 6th grade.</li>
+          </ul>
+        </div>
+      </section>
 
-//         <div className="experience-item">
-//           <h3>Middle School Teacher</h3>
-//           <h4>Tortolita Middle School</h4>
-//           <p>2015 - 2022</p>
-//           <ul>
-//             <li>Taught computers, TV broadcasting, and science.</li>
-//           </ul>
-//         </div>
+      <section>
+        <br></br>
+        <h2 className="text-2xl">Education</h2>
+        <div className="education-item">
+          <h3>Full Stack Bootcamp</h3>
+          <h4>The University of Arizona</h4>
+          <p>2022 - 2023</p>
+          <br></br>
+        </div>
+        <div className="education-item">
+          <h3>Bachelor of Arts in Education, 2001</h3>
+          <h3>STEM Teacher Certification, 2019</h3>
+          <h3>2nd Language Teacher Certification, 2005</h3>
+          <h3>Middle School Science, 2015</h3>
+          <h4>The University of Arizona</h4>
+          <br></br>
+        </div>
+      </section>
 
-//         <div className="experience-item">
-//           <h3>6th Grade Teacher</h3>
-//           <h4>Ironwood Elementary School</h4>
-//           <p>January 2019 - May 2019</p>
-//           <ul>
-//             <li>Taught most subject in 6th grade.</li>
-//           </ul>
-//         </div>
-//       </section>
+    </div>
+  );
+};
 
-//       <section>
-//         <h2>Education</h2>
-//         <div className="education-item">
-//           <h3>Full Stack Bootcamp</h3>
-//           <h4>The University of Arizona</h4>
-//           <p>2022 - 2023</p>
-//         </div>
-//         <div className="education-item">
-//           <h3>Bachelor of Arts in Education, 2001</h3>
-//           <h3>STEM Teacher Certification, 2019</h3>
-//           <h3>2nd Language Teacher Certification, 2005</h3>
-//           <h3>Middle School Science, 2015</h3>
-//           <h4>The University of Arizona</h4>
-//         </div>
-//       </section>
-
-//     </div>
-//   );
-// };
-
-// export default Resume;
+export default Resume;
 
 
 
