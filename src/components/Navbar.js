b49e12367d2829e7a5fcee3b8cd10da36539eccb
CF <cfreitas12345@gmail.com>
2023-06-24 22:46:43 -0700

    Tried out a new format, but  returned to original

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Logo from "../images/cfLogo.png";
 
-// https://v1.tailwindcss.com/components/navigation
+// https://www.material-tailwind.com/docs/react/navbar
+// https://reactjsexample.com/a-navbar-with-context-app-built-with-react-js/
 
 export default function Navbar({ handlePageChange, currentPage }) {
   console.log(currentPage);
@@ -9,7 +10,6 @@ export default function Navbar({ handlePageChange, currentPage }) {
     <nav className="flex items-center md:justify-between flex-wrap bg-stone-100 p-6 sticky top-0 font-extrabold tracking-wider">
       <div className="logo-wrapper">
         <div className="flex items-center flex-shrink-0 text-blueGray-900 mr-6">
-          {/* <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg> */}
           <div className="w-1/12 h-auto">
             <img src={Logo} alt="Logo with CF on a laptop screen."></img>
           </div>
@@ -78,3 +78,107 @@ export default function Navbar({ handlePageChange, currentPage }) {
     </nav>
   );
 }
+
+// import React from "react";
+// import {
+//   Navbar,
+//   Collapse,
+//   Typography,
+//   IconButton,
+// } from "@material-tailwind/react";
+// import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+// function NavList() {
+//   return (
+//     <ul className="my-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+//       <Typography
+//         as="li"
+//         variant="small"
+//         color="blue-gray"
+//         className="p-1 font-medium"
+//       >
+//         <a href="#" className="flex items-center hover:text-blue-500 transition-colors">
+//           About Me
+//         </a>
+//       </Typography>
+//       <Typography
+//         as="li"
+//         variant="small"
+//         color="blue-gray"
+//         className="p-1 font-medium"
+//       >
+//         <a href="#" className="flex items-center hover:text-blue-500 transition-colors">
+//           Portfolio
+//         </a>
+//       </Typography>
+//       <Typography
+//         as="li"
+//         variant="small"
+//         color="blue-gray"
+//         className="p-1 font-medium"
+//       >
+//         <a href="#" className="flex items-center hover:text-blue-500 transition-colors">
+//           My YouTube Channel
+//         </a>
+//       </Typography>
+//       <Typography
+//         as="li"
+//         variant="small"
+//         color="blue-gray"
+//         className="p-1 font-medium"
+//       >
+//         <a href="#" className="flex items-center hover:text-blue-500 transition-colors">
+//           Contact
+//         </a>
+//       </Typography>
+//     </ul>
+//   );
+// }
+
+// export default function Example() {
+//   const [openNav, setOpenNav] = React.useState(false);
+
+//   const handleWindowResize = () =>
+//     window.innerWidth >= 960 && setOpenNav(false);
+
+//   React.useEffect(() => {
+//     window.addEventListener("resize", handleWindowResize);
+
+//     return () => {
+//       window.removeEventListener("resize", handleWindowResize);
+//     };
+//   }, []);
+
+//   return (
+//     <Navbar className="mx-auto max-w-screen-xl px-6 py-3">
+//       <div className="flex items-center justify-between text-blue-gray-900">
+//         <Typography
+//           as="a"
+//           href="#"
+//           variant="h6"
+//           className="mr-4 cursor-pointer py-1.5"
+//         >
+//           Material Tailwind
+//         </Typography>
+//         <div className="hidden lg:block">
+//           <NavList />
+//         </div>
+//         <IconButton
+//           variant="text"
+//           className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+//           ripple={false}
+//           onClick={() => setOpenNav(!openNav)}
+//         >
+//           {openNav ? (
+//             <XMarkIcon className="h-6 w-6" strokeWidth={2} />
+//           ) : (
+//             <Bars3Icon className="h-6 w-6" strokeWidth={2} />
+//           )}
+//         </IconButton>
+//       </div>
+//       <Collapse open={openNav}>
+//         <NavList />
+//       </Collapse>
+//     </Navbar>
+//   );
+// }
