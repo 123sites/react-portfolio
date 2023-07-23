099a9db5752c06fe940557b59efa95c2be0ac2c2
CF <cfreitas12345@gmail.com>
2023-07-22 22:57:46 -0700

    Added footer margin and padding to improve the look of the icon hover & active highlighting.

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -5,16 +5,16 @@ const Footer = () => {
     return (
         <footer className="text-center text-stone-100 ">
             <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/chelwebdeveloper/">
-                <i className="fab fa-linkedin m-2 scale-150 px-1 rounded hover:bg-orange-500 active:bg-orange-500"></i>
+                <i className="fab fa-linkedin m-3 scale-150 p-1 rounded hover:bg-orange-500 active:bg-orange-500"></i>
             </a>
             <a target="_blank" rel="noopener noreferrer" href="https://github.com/123sites?tab=repositories">
-                <i className="fab fa-github m-2 scale-150 px-1 rounded hover:bg-orange-500 active:bg-orange-500"></i>
+                <i className="fab fa-github m-3 scale-150 p-1 rounded hover:bg-orange-500 active:bg-orange-500"></i>
             </a>
             <a target="_blank" rel="noopener noreferrer" href="mailto:">
-                <i className="fas fa-envelope m-2 scale-150 px-1 rounded hover:bg-orange-500 active:bg-orange-500"></i>
+                <i className="fas fa-envelope m-3 scale-150 p-1 rounded hover:bg-orange-500 active:bg-orange-500"></i>
             </a>
             <a className="downloadLink" href={resume} target="_blank" rel="noopener noreferrer">
-                <i className="fas fa-file-pdf m-2 scale-150 px-1 rounded hover:bg-orange-500 active:bg-orange-500"></i>
+                <i className="fas fa-file-pdf m-3 scale-150 p-1 rounded hover:bg-orange-500 active:bg-orange-500"></i>
             </a>
             <p className="text-center m-4 pb-3">2023 Copyright © Chel Freitas</p>
         </footer>
